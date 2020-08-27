42af151dc57b2a0c645a8fd45b5506a452997f73
Carolina Teixeira <carolina.txrs@gmail.com>
2020-08-27 20:17:54 -0300

    estruturs básica hmtl5 com script js

---
+++ js/main.js
@@ -0,0 +1,82 @@
+	
+/*	var nome = "Carolina Teixeira";
+	var n1 = 5;
+	var n2 = 10;
+
+	var frase = "Japão é o melhor time do mundo";
+
+	// alert(nome + " tem "+ n1 + " anos.");
+	// alert(n1 + n2);
+
+	console.log(nome);
+	console.log(n1 + n2);
+
+	console.log(frase);
+	// trocar valor
+	console.log(frase.replace("Japão","Brasil"));
+	// letra maiscula
+	// console.log(frase.toUpperCase());
+	// letra minuscula
+	console.log(frase.toLowerCase());
+	console.log(n1 * n2);
+*/
+	// Array
+
+	// var lista = ["maçã", "pera", "laranja"];
+	// adiciona elemento no array
+	// lista.push("uva");
+	// remove ultimo elemento do array
+	// lista.pop();
+	// tamanho da lista
+	// console.log(lista.length);
+	// reverter posições da lista
+	// console.log(lista.reverse());
+	// transorma em string
+	// console.log(lista);
+	// transforma em string 
+	// console.log(lista.toString());
+	// formata
+	// console.log(lista.join("-"));
+
+	// Dicionario
+	/*var fruta = {nome:"maça", cor:"vermelha"}
+	console.log(fruta.cor);
+	alert(fruta.cor);*/
+
+	// lista de Dicionario
+	/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxo"}]
+	console.log(frutas);
+	alert(frutas[1].nome);*/
+
+	// condicionais - laços de repeticao e date
+	
+	// if - else
+	/*var idade = prompt("Qual é sua idade?");
+	// var idade = 18;
+	if (idade >= 18){
+		alert("Maior de idade");
+	}else{
+		alert("menor de idade");
+	};*/
+
+	// while
+	/*var count = 0;
+	while (count <= 5){
+		console.log(count);
+		alert(count);
+		count++;
+	};*/
+
+	// for
+	/*var cont;
+	for (cont = 0; cont <= 5; cont++){
+		alert(cont);
+	};*/
+
+	// date
+	/*var d = new Date();
+	// alert(d);
+	// alert(d.getMonth()+1);
+	// alert(d.getMinutes());
+	alert(d.getDay());
+	alert(d.getHours());*/
